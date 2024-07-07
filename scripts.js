ed54a4cd08243634044161b508511ba622553b9b
Anjali Shah <95963877+anjaliyay@users.noreply.github.com>
2024-07-08 02:58:04 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcom.classList.toggle('bx-x');
+    navbar.classlist. toggle('active');
+}
